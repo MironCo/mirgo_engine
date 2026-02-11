6cd7a9aeac4f1fda8c4a669a2b7f5c6ca159dbdc
MironCo <masulicz@gmail.com>
2026-02-11 16:24:18 -0500

    Fixed Directory

--- cmd/test3d/main.go
+++ cmd/test3d/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"test3d/internal/game"
 )
 
 func main() {
-	// Change working directory to executable location
-	// This ensures assets are found on all platforms
+	// Change working directory to executable location for deployed builds.
+	// Skip this for "go run" which puts the binary in a temp directory.
 	if execPath, err := os.Executable(); err == nil {
 		execDir := filepath.Dir(execPath)
-		if err := os.Chdir(execDir); err != nil {
-			fmt.Printf("Warning: could not change to executable directory: %v\n", err)
+		// Detect "go run" by checking if executable is in a temp/go-build directory
+		if !strings.Contains(execDir, "go-build") {
+			os.Chdir(execDir)
 		}
 	}
 
